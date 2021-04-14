554dd2148953eaae1c130371d6731424b746e90a
hamjeehyun <2075jh@gmail.com>
2021-04-14 22:42:34 +0900

    숨바꼭질2 21-04-14

---
+++ baekjoon/12851.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+input=sys.stdin.readline
+MAX_SIZE=100001
+
+n,k=map(int, input().split())
+
+cnt=0
+check=[-1]*MAX_SIZE
+check[n]=0
+
+queue=deque()
+queue.append(n)
+
+while queue:
+    now=queue.popleft()
+
+    if now==k:
+        cnt+=1
+
+    for next_ in [now*2, now+1, now-1]:
+        if 0 <= next_ < MAX_SIZE:
+            if check[next_]==-1 or check[next_]>=check[now]+1:
+                check[next_]=check[now]+1
+                queue.append(next_)
+
+print(check[k])
+print(cnt)
