81630e83e2a02dc7ad21da15ce3e2dceb35951d7
hamjeehyun <2075jh@gmail.com>
2021-04-17 20:36:42 +0900

    점프 21-04-17

---
+++ baekjoon/1890.py
@@ -0,0 +1,25 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+mat=[]
+for _ in range(n):
+    mat.append(list(map(int, input().split())))
+
+dp=[[0 for _ in range(n)] for _ in range(n)]
+dp[0][0]=1
+
+for x in range(n):
+    for y in range(n):
+        if mat[x][y]==0:
+            break
+
+        nx=x+mat[x][y]
+        ny=y+mat[x][y]
+
+        if 0<=nx<n:
+            dp[nx][y]+=dp[x][y]
+        if 0<=ny<n:
+            dp[x][ny]+=dp[x][y]
+
+print(dp[-1][-1])
