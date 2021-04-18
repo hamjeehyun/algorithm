b1e30d720c8e46863f175781e4c39e02eb2f9c0b
hamjeehyun <2075jh@gmail.com>
2021-04-18 20:19:46 +0900

    기타리스트 21-04-18

---
+++ baekjoon/1495.py
@@ -0,0 +1,25 @@
+import sys
+input=sys.stdin.readline
+
+n, s, m=map(int, input().split())
+volumn=list(map(int, input().split()))
+dp=[[0] * (m+1) for _ in range(n+1)]
+
+dp[0][s]=1
+
+for i in range(1, n+1):
+    for j in range(m+1):
+        if dp[i-1][j]==0:
+            continue
+        if j+volumn[i-1] <= m:
+            dp[i][j+volumn[i-1]]=1
+
+        if j-volumn[i-1] >= 0:
+            dp[i][j-volumn[i-1]]=1
+
+result=-1
+for j in range(m, -1, -1):    
+    if dp[n][j] == 1:
+        result=j
+        break
+print(result)
