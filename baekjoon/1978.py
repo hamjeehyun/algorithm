5548fcdaaf8a7ef69a8eea40394e487de0a73c71
hamjeehyun <2075jh@gmail.com>
2021-05-08 22:15:11 +0900

    소수찾기 21-05-08

--- baekjoon/1978.py
+++ baekjoon/1978.py
@@ -1,20 +1,20 @@
 def is_prime(n):
     # 소수면 True
-    if n==1:
+    if n<2:
         return False
-    for i in range(2, n):
+    i=2
+    while i*i <= n:
         if n%i == 0:
             return False
+        i += 1
     return True
 
 n=int(input())
 arr=list(map(int, input().split()))
 
 cnt=0
-
-for i in range(n):
-    result=is_prime(arr[i])
-    if result == True:
-        cnt+=1
+    
+for a in arr:
+    if is_prime(a): cnt += 1
 
 print(cnt)
