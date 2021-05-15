373fd0edd77d0ff3088b93219e2ebe85d556056e
hamjeehyun <2075jh@gmail.com>
2021-05-15 14:54:49 +0900

    N과 M(2) 21-05-15

--- baekjoon/15649.py
+++ baekjoon/15649.py
@@ -12,15 +12,15 @@ def go(index, n, m):
         if c[i]: 
             continue # 사용한 것이라면 넘기기
 
-        c[i] = True # 사용
+        visited[i] = True # 사용
         arr.append(i+1) # arr 입력
         go(index+1, n, m)
-        c[i] = False
+        visited[i] = False
         arr.pop()
 
     
 n, m = map(int, input().split())
-c = [False] * 10
+visited = [False] * 10
 arr =[]
 
 # 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
--- baekjoon/15650.py
+++ baekjoon/15650.py
@@ -2,7 +2,17 @@ import sys
 input = sys.stdin.readline
 
 def go(index, start, n, m):
+    if index == m:
+        print(' '.join(map(str, arr)))
+        return
 
+    for i in range(start, n):
+        arr.append(i+1)
+
+        go(index+1, i+1, n, m)
+        arr.pop()
 
 n, m = map(int, input().split())
-visited = [False] * 10
+arr = []
+
+go(0, 0, n, m)
