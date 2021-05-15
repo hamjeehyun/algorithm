d79844a39ae424d6ade2d071f16e07ec206a0892
hamjeehyun <2075jh@gmail.com>
2021-05-15 16:13:39 +0900

    N과 M(6) 21-05-15

--- baekjoon/15655.py
+++ baekjoon/15655.py
@@ -1,1 +1,28 @@
- 
+import sys
+input = sys.stdin.readline
+
+def go(index, start, n, m):
+    if index == m:
+        print(' '.join(map(str, result)))
+        return 
+    
+    for i in range(start, n):
+        if visited[i]:
+            continue
+
+        visited[i] = True
+        result.append(arr[i])
+
+        go(index+1, i+1, n, m)
+
+        result.pop()
+        visited[i] = False
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+result = []
+visited=[False] * n
+
+go(0, 0, n, m)
