c888c841f1adc556b26b4cff473020a6648973ec
hamjeehyun <2075jh@gmail.com>
2021-06-20 11:30:28 +0900

    problem77 21-06-20

---
+++ coderpad-interviews/problem77.py
@@ -0,0 +1,16 @@
+
+arr = [(1, 3), (5, 8), (4, 10), (20, 25)]
+
+def merge(arr):
+    sort_list = sorted(arr)
+    merge_list = []
+
+    for item in sort_list:
+        if merge_list and item[0] < merge_list[-1][1]:
+            merge_list[-1] = (merge_list[-1][0], max(item[1], merge_list[-1][1]))
+        else:
+            merge_list.append(item)
+
+    return merge_list
+
+print(merge(arr))
