db083db1bf6c0097c69a0d7503062f518ffebd84
hamjeehyun <2075jh@gmail.com>
2021-08-01 21:40:47 +0900

    21-08-01 쉬운 계단 수

---
+++ baekjoon/10844.py
@@ -0,0 +1,21 @@
+mod = 1000000000 # 나눌 수
+d = [[0] * 10 for _ in range(101)] # n <= 100 
+n = int(input())
+
+for i in range(1, 10): # 초기화
+    d[1][i] = 1 
+
+for i in range(2, n+1):
+    for j in range(10):
+        d[i][j] = 0
+        if j-1 >= 0:
+            d[i][j] += d[i-1][j-1]
+
+        if j+1 <= 9:
+            d[i][j] += d[i-1][j+1]
+        d[i][j] %= mod
+
+ans = 0
+for i in range(10):
+    ans += d[n][i]
+print(ans%mod)
